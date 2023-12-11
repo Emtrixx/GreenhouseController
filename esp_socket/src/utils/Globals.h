6d3c7ab5c73b9153fb52406b9a2a9bc58324b92c
Jesse Guenzl <contact@jesseguenzl.com>
2023-12-11 20:11:46 +0200

    comment out atomics in globals, replace with normal types

--- esp_socket/src/utils/Globals.h
+++ esp_socket/src/utils/Globals.h
@@ -8,7 +8,7 @@
 #ifndef UTILS_GLOBALS_H_
 #define UTILS_GLOBALS_H_
 
-#include <atomic>
+//#include <atomic>
 
 #include "FreeRTOS.h"
 #include "semphr.h"
@@ -17,11 +17,16 @@
 
 typedef struct {
     QueueHandle_t rotaryEncoderQueue;
-    std::atomic<int> co2level;
-    std::atomic<int> humidity;
-    std::atomic<int> temperature;
-    std::atomic<float> valveOpeningPercentage;
-    std::atomic<int> co2Target;
+//    	std::atomic<int> co2level;
+    	int co2level;
+    	int humidity;
+    	int temperature;
+    	float valveOpeningPercentage;
+    	int co2Target;
+//    std::atomic<int> humidity;
+//    std::atomic<int> temperature;
+//    std::atomic<float> valveOpeningPercentage;
+//    std::atomic<int> co2Target;
 } GlobalStruct_t;
 
 extern GlobalStruct_t globalStruct;
